ae60df7d54fe1fa5c9d5ca225af6ce0e077909e4
Simon van der Veldt <simon.vanderveldt@gmail.com>
2016-08-26 14:31:41 +0200

    Release v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fleet-service',
-    version='0.0.2',
+    version='0.0.3',
     url='http://github.com/simonvanderveldt/fleet-service',
     author='Simon van der Veldt',
     packages=[],
