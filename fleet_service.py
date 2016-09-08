172d9bd953c3eccc3e53f1aa821776f026e23172
Simon van der Veldt <simon.vanderveldt@gmail.com>
2016-09-08 17:00:51 +0200

    Add destroy command

--- fleet_service.py
+++ fleet_service.py
@@ -94,6 +94,51 @@ class FleetService(fleet_helper.FleetHelper):
             self.destroy_unit(instance)
 
 
+    def destroy_service(self, service_name):
+        """Destroy a service"""
+        template_unit_name = service_name + '@.service'
+
+        # Get all units so we can find the template and instances of this service
+        self.get_units()
+
+        # Destroy old template if it exists
+        if any(unit['name'] == template_unit_name for unit in self.fleet_units):
+            self.logger.info('Destroying old template ' + template_unit_name)
+            self.destroy_unit(template_unit_name)
+
+        # Get existing instances of this service
+        self.existing_service_instances = sorted(self.get_unit_instances(service_name))
+        if not self.existing_service_instances:
+            self.logger.info('There are no instances for this service, exiting')
+            raise SystemExit()
+        self.logger.info('Existing instance count: ' + str(len(self.existing_service_instances)))
+        self.logger.info('Existing instances: ' + str(self.existing_service_instances))
+
+        # Get existing instances we manage
+        our_unit_instance_pattern = re.compile(r"^" + re.escape(service_name) + r"@(\d+)\.service")
+        self.our_existing_service_instances = sorted([unit for unit in self.existing_service_instances if our_unit_instance_pattern.match(unit)])
+        self.logger.info('Existing instances managed by us: ' + str(self.our_existing_service_instances))
+
+        # Destroy non-instanced unit if it exists
+        non_instanced_unit_name = service_name + '.service'
+        if any(unit['name'] == non_instanced_unit_name for unit in self.fleet_units):
+            self.logger.warning('Destroying non-instance unit ' + non_instanced_unit_name)
+            self.destroy_unit(non_instanced_unit_name)
+
+        # Destroy instances we don't manage
+        self.wrong_instance_units = sorted(set(self.existing_service_instances) - set(self.our_existing_service_instances))
+        if len(self.wrong_instance_units) > 0:
+            self.logger.warning('Destroying non-matching instance units ' + str(self.wrong_instance_units))
+        for unit in self.wrong_instance_units:
+            self.destroy_unit(unit)
+
+        # Destroy instances we manage
+        if len(self.our_existing_service_instances) > 0:
+            self.logger.info('Destroying instances: ' + str(self.our_existing_service_instances))
+        for instance in sorted(self.our_existing_service_instances, reverse=True):
+            self.destroy_unit(instance)
+
+
     def ps(self):
         """Return service state"""
         instances = {}
